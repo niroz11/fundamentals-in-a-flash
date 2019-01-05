7aa4f12a3c3f34efcb986a991af9759d997b87d8
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-04 18:10:26 -0700

    Update testing for welcome component

--- src/tests/Welcome.test.js
+++ src/tests/Welcome.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Welcome from '../Welcome';
 import { shallow } from 'enzyme';
 
-
+const updateCategoryMock = jest.fn();
 describe('Welcome', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
       <Welcome
+        updateCategory={updateCategoryMock}
       />
     );
   });
@@ -16,4 +17,13 @@ describe('Welcome', () => {
   it('should match the snapshot with all data passed in', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should have a default state', () => {
+    expect(wrapper.state()).toEqual({ categories: ['Prototype Methods', 'Scope', 'OOP'] });
+  });
+
+  it.skip('should update category when a category is clicked', () => {
+    wrapper.find('color-0').simulate('click')
+  });
+
 });
