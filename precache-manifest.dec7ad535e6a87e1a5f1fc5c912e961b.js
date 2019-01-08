18c5b6781589d5c1ed85a6b5588fe5402b312d93
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-08 15:45:16 -0700

    Updates

---
+++ precache-manifest.dec7ad535e6a87e1a5f1fc5c912e961b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0cceecd88d05d8905b",
+    "url": "/fundamentals-in-a-flash/static/js/runtime~main.de0cceec.js"
+  },
+  {
+    "revision": "1d3acc5f30df4b6c26fd",
+    "url": "/fundamentals-in-a-flash/static/js/main.1d3acc5f.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/fundamentals-in-a-flash/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "1d3acc5f30df4b6c26fd",
+    "url": "/fundamentals-in-a-flash/static/css/main.3417540a.chunk.css"
+  },
+  {
+    "revision": "0cda4fcf6261e6e0c1d2c9bf3cc9d517",
+    "url": "/fundamentals-in-a-flash/index.html"
+  }
+];
