1632844ce3c9784be989dd59c23376e2ba2352e6
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-07 17:39:06 -0700

    Add testing for message component

--- src/Message.js
+++ src/Message.js
@@ -13,29 +13,25 @@ class Messsage extends Component {
     this.props.updateUserStatus('finished')
   }
 
-  moveToNextQuestion = (event) => {
-    event.preventDefault();
-    this.props.moveToNextQuestion();
-  }
-
   render() {
-    let { id, category, question, answers, correct_answer, resources } = this.props.currentQuestion;
+    let { userStatus, currentQuestion, updateUserStatus, moveToNextQuestion, isFinalQuestion } = this.props;
+    let { id, category, question, answers, correct_answer, resources } = currentQuestion;
     let style = category.toLowerCase().split(' ').join('-')
     return (
       <div className={"message-contain " + style}>
       {
-        this.props.userStatus === 'correct' && 
+        userStatus === 'correct' && 
           <p>Yay! <span className="answer">{correct_answer}</span> is the correct answer</p>
       }
       {
-        this.props.userStatus === 'wrong' && 
+        userStatus === 'wrong' && 
           <p><span className="sorry">Sorry that is not correct.</span>Check out the <a href={resources} target="_blank">docs</a> for more information</p>
       }
       {
-          !this.props.isFinalQuestion && <button className="next-btn" onClick={this.moveToNextQuestion}>Next</button>
+          !isFinalQuestion && <button className="next-btn" onClick={moveToNextQuestion}>Next</button>
       }
       {
-          this.props.isFinalQuestion &&  <button className="finish-btn" onClick={this.updateUserStatus}>Finish</button>
+          isFinalQuestion &&  <button className="finish-btn" onClick={this.updateUserStatus}>Finish</button>
       }
       </div>
     )
--- src/tests/Message.test.js
+++ src/tests/Message.test.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Message from '../Message';
 import { shallow } from 'enzyme';
 
+const userStatus = 'correct';
+const currentQuestion = {
+  id: "17",
+  category: "OOP",
+  question: "When creating a new class that extends from a parent class, what keyword is used to access and invoke methods from the parent class?",
+  answers: [ "function", "borrow", "super"],
+  correct_answer: "super",
+  resources: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super"
+}
+const updateUserStatusMock = jest.fn();
+const moveToNextQuestionMock = jest.fn();
+// const isFinalQuestion = false;
 
 describe('Message', () => {
   let wrapper;
@@ -9,11 +21,26 @@ describe('Message', () => {
   beforeEach(() => {
     wrapper = shallow(
       <Message
+        userStatus={userStatus} 
+        currentQuestion={currentQuestion} 
+        updateUserStatus={updateUserStatusMock} 
+        moveToNextQuestion={moveToNextQuestionMock} 
+        isFinalQuestion={false}
       />
     );
   });
 
-  it.skip('should match the snapshot with all data passed in', () => {
+  it('should match the snapshot with all data passed in', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should move to the next question', () => {
+    wrapper.find('button.next-btn').simulate('click');
+    expect(moveToNextQuestionMock).toHaveBeenCalled();
+  });
+
+  it('should update state to finished', () => {
+    wrapper.instance().updateUserStatus({preventDefault: () => {}})
+    expect(updateUserStatusMock).toHaveBeenCalledWith('finished');
   });
 });
